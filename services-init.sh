a202f24edfc9abd18baefd6df25239bdb93a8599
Nate Sales <nate@nate.to>
2020-02-08 22:56:56 -0800

    Update services-init.sh

--- services-init.sh
+++ services-init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
 sudo dpkg-reconfigure tzdata
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
